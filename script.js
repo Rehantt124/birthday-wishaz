36b53f5d5c45b2ed2093903d0a0520375b72ea82
Rehantt124 <rehanrobert789@gmail.com>
2026-01-13 18:20:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const second = 1000,
   minute = second * 60,
   hour = minute * 60,
   day = hour * 24;
-let countDown = new Date('Jan 13, 2026 00:00:00').getTime(),
+let countDown = new Date('Oct 22, 2023 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -448,6 +448,7 @@ function confetti() {
 
   if (!onlyOnKonami) poof();
 };
+
 
 
 
